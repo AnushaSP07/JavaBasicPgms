111ca8c3438fbbd26f5e864485317a8079259365
Anusha SP <anushaspga@gmail.com>
2021-10-15 23:16:39 +0530

    Pattern

---
+++ com.day.one/Pattern.java
@@ -0,0 +1,25 @@
+package com.day.one;
+
+import java.util.Scanner;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Number of rows");
+		int num = sc.nextInt();
+		int rowsCount = 1;
+		for(int i=num;i>0;i--) {
+			for(int j=1;j<i;j++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=rowsCount;j++) {
+				System.out.print(rowsCount+" ");
+			}
+			System.out.println();
+			rowsCount++;
+		}
+		sc.close();
+	}
+
+}
